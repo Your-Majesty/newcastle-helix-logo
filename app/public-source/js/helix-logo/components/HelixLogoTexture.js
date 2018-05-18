d9a4256fd594415e104e9b2948a1d51c8e3cdf28
Abril Alvarez <>
2018-05-18 17:26:25 +0200

    Added color in beetween

--- app/public-source/js/helix-logo/components/HelixLogoTexture.js
+++ app/public-source/js/helix-logo/components/HelixLogoTexture.js
@@ -14,7 +14,7 @@ class HelixLogoTexture {
     this.variationRatio = 0.0058
     this.noiseSize = 180.5
     this.colorBackground = false
-    this.coloredDivisions = false
+    this.coloredDivisions = true
     this.colorTransparency = false
     this.breakSize = 0.3
     this.breakFrequency = 10.0
@@ -91,7 +91,7 @@ class HelixLogoTexture {
       this.monochromeColor = 0.    
       this.coloredDivisions = false
     } else {
-      this.coloredDivisions = false
+      this.coloredDivisions = true
       this.isMonochrome = false
 
     }
