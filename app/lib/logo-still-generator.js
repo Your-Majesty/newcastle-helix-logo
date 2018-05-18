c6d35732883f42f67cab2af559a757aca0ad51b5
Abril Alvarez <>
2018-05-18 17:58:58 +0200

    Going back to logo random

--- app/lib/logo-still-generator.js
+++ app/lib/logo-still-generator.js
@@ -28,7 +28,6 @@ module.exports = (() => {
   ]
 
   controller.captureCurrentState = async () => {
-    
     await fs.ensureDir(`${__dirname}/../public/latest/`)
     await fs.ensureDir(`${__dirname}/../tmp/`)
 
