f2b599aefe5a4105f9249d99bb062da79a39dd74
Abril Alvarez <>
2018-06-05 17:44:02 +0200

    Added camera tracker

---
+++ app/public-source/js/helix-logo/components/HelixLogoUIScale.js
@@ -0,0 +1,9 @@
+class HelixLogoUIScale {
+  constructor(args) {
+    this.element = document.querySelector('.helix-logo-ui-scale-buttons')
+    this.buttonPlus = this.element.querySelector('.helix-logo-ui-scale-more')
+    this.buttonMinus = this.element.querySelector('.helix-logo-ui-scale-less')
+
+  }
+
+}
---
+++ app/public-source/js/helix-logo/services/CameraTracker.js
@@ -0,0 +1,13 @@
+const CameraTracker = (() => {
+  const controller = {}
+
+  controller.zoomIn = () => {
+    console.log('Something was done.')
+  }
+
+  controller.zoomOut = () => {
+    console.log('Something was done.')
+  }
+
+  return controller
+})()
