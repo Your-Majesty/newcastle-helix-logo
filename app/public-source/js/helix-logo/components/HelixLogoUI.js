68cd34840bbba6484f914e4c08367f19a1c3e834
Abril Alvarez <>
2018-06-08 16:35:44 +0200

    Remove animateIn

--- app/public-source/js/helix-logo/components/HelixLogoUI.js
+++ app/public-source/js/helix-logo/components/HelixLogoUI.js
@@ -19,8 +19,6 @@ class HelixLogoUI {
     this.animateOut = this.animateOut.bind(this)
     this.animateOverlayOut = this.animateOverlayOut.bind(this)
 
-    this.animateIn()
-
     this.buttons.buttons.addEventListener('uiButtonPressed', (e) => {
       if (!this.sliderIsActive) {
         this.showSliders()
