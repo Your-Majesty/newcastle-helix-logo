e15b5489b65d94cbad1c897fb32eda5d8b93159e
Abril Alvarez <>
2018-06-20 14:35:16 +0200

    Updated IE11 timeline

--- app/public-source/js/helix-logo/components/HelixLogoUI.js
+++ app/public-source/js/helix-logo/components/HelixLogoUI.js
@@ -24,7 +24,6 @@ class HelixLogoUI {
     this.animateOut = this.animateOut.bind(this)
     this.animateOverlayOut = this.animateOverlayOut.bind(this)
 
-    this.animateIn()
     this.buttons.buttons.addEventListener('uiButtonPressed', (e) => {
       if (!this.sliderIsActive) {
         this.showSliders()
